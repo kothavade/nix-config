8cdb034135d5d232cb44582c75107622eb0a6fc0
Ved Kothavade <ved@kothavade.com>
2024-03-22 23:10:46 -0700

    kitty: cleanup settings

--- home/common/kitty/default.nix
+++ home/common/kitty/default.nix
@@ -6,30 +6,37 @@
     settings.shell = "${pkgs.fish}/bin/fish";
     # settings.shell = "/etc/profiles/per-user/ved/bin/fish";
     extraConfig = ''
-      font_family Liga  SFMono Nerd Font
+      # base
+      editor  nvim
+
+      # font
+      font_family       Liga SFMono Nerd Font
       bold_font         Liga SFMono Nerd Font Bold
-      italic_font Liga  SFMono Nerd Font Italic
+      italic_font       Liga SFMono Nerd Font Italic
       bold_italic_font  Liga SFMono Nerd Font Bold Italic
       modify_font       underline_thickness 1
       modify_font       underline_position 18
-
+      font_size         12.0
       disable_ligatures cursor
 
-      font_size 12.0
+      # mac
+      macos_option_as_alt                 yes
+      macos_quit_when_last_window_closed  yes
 
-      macos_option_as_alt yes
-      macos_quit_when_last_window_closed yes
+      # hotkeys
+      map   cmd+t             new_tab_with_cwd
+      map   kitty_mod+t       new_tab_with_cwd
+      map   kitty_mod+enter   launch --cwd=current --type=window
+      map   cmd+enter         launch --cwd=current --type=window
+      map   kitty_mod+e       kitten hints
+      map   cmd+e             kitten hints
 
-      map cmd+t new_tab_with_cwd
-      map kitty_mod+t new_tab_with_cwd
-      map kitty_mod+enter launch --cwd=current --type=window
-      map cmd+enter       launch --cwd=current --type=window
+      # click on links to open
       mouse_map left click ungrabbed mouse_handle_click selection link prompt
-      map kitty_mod+e kitten hints
-      map cmd+e kitten hints
-      include gruvbox.conf
 
-      tab_bar_style powerline
+      # ui
+      tab_bar_style   powerline
+      include         gruvbox.conf
     '';
   };
   home.file.".config/kitty/gruvbox.conf".source = ./gruvbox.conf;
