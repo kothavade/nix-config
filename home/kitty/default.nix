05dfd606bc7ff1d05f2a6a2d1a26a702c1a7e032
Ved Kothavade <ved@kothavade.com>
2024-05-21 02:27:49 -0400

    kitty ctrl on linux

--- home/kitty/default.nix
+++ home/kitty/default.nix
@@ -9,6 +9,7 @@ _: {
 
       # font
       font_family       Berkeley Mono
+      symbol_map        U+e000-U+e00a,U+ea60-U+ebeb,U+e0a0-U+e0c8,U+e0ca,U+e0cc-U+e0d4,U+e200-U+e2a9,U+e300-U+e3e3,U+e5fa-U+e6b1,U+e700-U+e7c5,U+f000-U+f2e0,U+f300-U+f372,U+f400-U+f532,U+f0001-U+f1af0 Symbols Nerd Font Mono
       symbol_map        U+279c Arial Unicode MS
       modify_font       underline_thickness 1
       modify_font       underline_position 18
@@ -21,12 +22,17 @@ _: {
 
       # hotkeys
       map   cmd+t             new_tab_with_cwd
+      map   ctrl+t             new_tab_with_cwd
       map   cmd+l             next_layout
-      map   kitty_mod+l             next_layout
+      map   ctrl+l             next_layout
+      map   kitty_mod+l       next_layout
       map   kitty_mod+t       new_tab_with_cwd
+      map   ctrl+t       new_tab_with_cwd
       map   kitty_mod+enter   launch --cwd=current --type=window
+      map   ctrl+enter        launch --cwd=current --type=window
       map   cmd+enter         launch --cwd=current --type=window
       map   kitty_mod+e       kitten hints
+      map   ctrl+e       kitten hints
       map   cmd+e             kitten hints
 
       # click on links to open
