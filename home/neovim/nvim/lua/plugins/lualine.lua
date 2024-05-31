c52b7a7e217641366c8344a0ebb08cfea9415ce6
Ved Kothavade <ved@kothavade.com>
2024-05-31 03:35:54 -0400

    remove lualine theme override
    lazyvim updated trouble config

--- home/neovim/nvim/lua/plugins/lualine.lua
+++ home/neovim/nvim/lua/plugins/lualine.lua
@@ -1,19 +1,14 @@
 return {
   "nvim-lualine/lualine.nvim",
-  opts = function(_, opts)
-    local gruv = require("lualine.themes.gruvbox")
-    gruv.normal.c.bg = "#282828"
-    return {
-      options = {
-        theme = gruv,
-        section_separators = '',
-        component_separators = ''
-      },
-      extensions = { "lazy" },
-      sections = {
-        -- Remove clock from lualine
-        lualine_z = {},
-      },
-    }
-  end,
+  opts = {
+    options = {
+      section_separators = '',
+      component_separators = ''
+    },
+    extensions = { "lazy" },
+    sections = {
+      -- Remove clock from lualine
+      lualine_z = {},
+    },
+  }
 }
