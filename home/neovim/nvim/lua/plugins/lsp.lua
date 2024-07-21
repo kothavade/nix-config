b049cc6e53d0a672ac20a9cc766b87fcdc968652
Ved Kothavade <ved@kothavade.com>
2024-07-21 19:50:29 -0400

    fix lsp

--- home/neovim/nvim/lua/plugins/lsp.lua
+++ home/neovim/nvim/lua/plugins/lsp.lua
@@ -1,52 +1,56 @@
 return {
   "neovim/nvim-lspconfig",
   ---@class PluginLspOpts
-  opts = {
-    inlay_hints = {
-      enabled = true,
-    },
-    --@type lspconfig.options
-    servers = {
-      lua_ls = {
-        settings = {
-          Lua = {
-            format = {
-              enabled = true }
-          }
-        }
+  opts = function(_, opts)
+    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    keys[#keys + 1] = { "<M-n>", false }
+    return vim.tbl_deep_extend("force", opts, {
+      inlay_hints = {
+        enabled = true,
       },
-      nil_ls = {},
-      astro = {},
-      typst_lsp = {
-        root_dir = function()
-          return vim.fn.getcwd()
-        end,
-      },
-      ruff_lsp = {},
-      ocamllsp = {},
-      zls = {
-        cmd = { "/home/ved/code/zls/zig-out/bin/zls" },
-        settings = {
-          zls = {
-            path = "/home/ved/code/zls/zig-out/bin/zls",
-            enable_snippets = true,
-            enable_ast_check_diagnostics = true,
-            enable_autofix = true,
-            enable_import_embedfile_argument_completions = true,
-            warn_style = true,
-            enable_semantic_tokens = true,
-            enable_inlay_hints = true,
-            inlay_hints_hide_redundant_param_names = true,
-            inlay_hints_hide_redundant_param_names_last_token = true,
-            operator_completions = true,
-            include_at_in_builtins = true,
-            max_detail_length = 1048576,
+      --@type lspconfig.options
+      servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              format = {
+                enabled = true }
+            }
           }
         },
+        nil_ls = {},
+        astro = {},
+        typst_lsp = {
+          root_dir = function()
+            return vim.fn.getcwd()
+          end,
+        },
+        ruff_lsp = {},
+        ocamllsp = {},
+        zls = {
+          cmd = { "/home/ved/code/zls/zig-out/bin/zls" },
+          settings = {
+            zls = {
+              path = "/home/ved/code/zls/zig-out/bin/zls",
+              enable_snippets = true,
+              enable_ast_check_diagnostics = true,
+              enable_autofix = true,
+              enable_import_embedfile_argument_completions = true,
+              warn_style = true,
+              enable_semantic_tokens = true,
+              enable_inlay_hints = true,
+              inlay_hints_hide_redundant_param_names = true,
+              inlay_hints_hide_redundant_param_names_last_token = true,
+              operator_completions = true,
+              include_at_in_builtins = true,
+              max_detail_length = 1048576,
+            }
+          },
+        },
+        racket_langserver = {},
+        -- TODO: basedpyright
+        -- pyright = {},
       },
-      racket_langserver = {},
-      -- TODO: basedpyright
-      -- pyright = {},
-    },
-  },
+    })
+  end,
 }
